61aaa55c7f3326ae8af0a2ebdd761055cb099427
Ryan Sanders <ryan.sanders@build.com>
2025-03-26 17:58:48 -0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StagePlotCanvas from './components/StagePlotCanvas';
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <StagePlotCanvas />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/StagePlotCanvas.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StagePlotCanvas: React.FC = () => {
+  return (
+    <div className="w-full h-full border border-gray-400">
+      <p className="text-center mt-4">Stage Plot Canvas Placeholder</p>
+    </div>
+  );
+};
+
+export default StagePlotCanvas;
